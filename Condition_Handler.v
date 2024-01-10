638613badb8f3f8fb4964374a62c04d4f2739864
Detective Pikachu <frenzyexists@gmail.com>
2024-01-10 19:57:44 -0400

    Another minor fix on the condition handler, my bad

--- Condition_Handler.v
+++ Condition_Handler.v
@@ -2,50 +2,49 @@ module Condition_Handler (
     output reg          if_id_reset,
     output reg          CH_Out,
 
-    input wire  [5:0]   RT,
-    input wire          z_flag,
-    input wire          n_flag
+    input wire [5:0]    CH_opcode,
+    input wire [4:0]    RT,
+    input wire          Z_FLAG,
+    input wire          N_FLAG
 ); 
 
-
 always @* begin
-    
-    case (RT)
+    case (CH_opcode)
         6'b000101: begin //not equal
-            if(z_flag == 0) CH_Out <= 1'b1;
+            if(Z_FLAG == 0) CH_Out <= 1'b1;
             else CH_Out <= 1'b0;
         end
         6'b000100: begin //equal
-            if(z_flag == 1) CH_Out <= 1'b1;
+            if(Z_FLAG == 1) CH_Out <= 1'b1;
             else CH_Out <= 1'b0;
         end
         6'b000111: begin //greater than zero
-            if(instruction[20:16] == 5'b00000) begin
-                if((z_flag == 0) && (n_flag == 0)) CH_Out <= 1'b1;
+            if(RT == 5'b00000) begin
+                if((Z_FLAG == 0) && (N_FLAG == 0)) CH_Out <= 1'b1;
                 else CH_Out <= 1'b0;
             end
         end 
         6'b000110: begin //lesser or equal than zero
-            if(instruction[20:16] == 5'b00000) begin
-                if((z_flag == 1) || (n_flag == 1)) CH_Out <= 1'b1;
+            if(RT == 5'b00000) begin
+                if((Z_FLAG == 1) || (N_FLAG == 1)) CH_Out <= 1'b1;
                 else CH_Out <= 1'b0;
             end
         end
         6'b000001: begin
-            case (instruction[20:16])
+            case (RT)
               5'b10001: begin //BAL (BGEZAL) //branch and link
                     CH_Out <= 1'b1;
               end
              5'b00001 : begin //greater or equal than zero
-                    if((z_flag == 1) || (n_flag == 0)) CH_Out <= 1'b1;
+                    if((Z_FLAG == 1) || (N_FLAG == 0)) CH_Out <= 1'b1;
                     else CH_Out <= 1'b0;
               end
               5'b00000: begin //lesser than zero
-                    if((z_flag == 0) && (n_flag == 1)) CH_Out <= 1'b1;
+                    if((Z_FLAG == 0) && (N_FLAG == 1)) CH_Out <= 1'b1;
                     else CH_Out <= 1'b0;
               end
               5'b10000: begin //lesser than zero and link
-                    if(n_flag == 1) CH_Out <= 1'b1;
+                    if(N_FLAG == 1) CH_Out <= 1'b1;
                     else CH_Out <= 1'b0;
               end
             endcase
